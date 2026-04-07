61d6889ba0c07809ffaf71428269398ae7dbe23b
Naveen Rudra <naveenrudra311@example.com>
2026-04-07 19:23:57 +0530

    dd

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
-from .database import init_db
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from .resume_routes import router as resume_router
-from .ai_routes import router as ai_routerr
+from .ai_routes import router as ai_router
+
+load_dotenv()
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(resume_router)
 app.include_router(ai_router)
